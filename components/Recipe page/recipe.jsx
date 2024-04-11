4be57a07055aacb27bc1bbeb2020baf026d8bd6e
mazenaliii <125923701+mazenaliii@users.noreply.github.com>
2024-04-11 07:54:08 +0200

    Update recipe.jsx

--- components/Recipe page/recipe.jsx
+++ components/Recipe page/recipe.jsx
@@ -1,10 +1,6 @@
-'use client'
-import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const RecipePage = () => {
-  const router = useRouter()
-  router.push('/')
   return (
     <div>Nothing exists in this page.</div>
   )
